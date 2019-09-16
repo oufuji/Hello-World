0e4e93f3fa4a0d41a8e7fa7ac5b7ab0bc6f0edac
oufuji <48410484+oufuji@users.noreply.github.com>
2019-09-16 11:20:39 +0800

    build a news C file to tell World : I am coming!
    Build a news C file to tell World : I am coming!

---
+++ hello_world.c
@@ -0,0 +1,8 @@
+include<stdio.h>
+
+int main(int  argc,char **argv){
+    /* to use my first function : printf(char* ,...); */
+    printf("Hello world!");
+    /* return 0 to PC for tell PC run within errors!*/
+    return 0;
+}
